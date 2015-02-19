9cea739f4d0106622b605df4856eb7e0f49cabfa
Abrahamon <ipseabraham@gmail.com>
2015-02-19 17:35:41 -0600

    lista con eliminar
    List

--- lista.h
+++ lista.h
@@ -1,38 +1,22 @@
-/*
- *
- */
-
 #ifndef LISTA_H
 #define LISTA_H
 
 #include "nodo.h"
 
-template <class T> class Lista
+class lista
 {
 protected:
-    Nodo<T>* cabeza = NULL;
+    nodo* cabeza;
+    nodo* tmp;
+    nodo* current;
 
 public:
-    Lista();
-    void crearLista();
-    void insertar(T* pData);
-    void visualizar();
+    lista();
+
+    void insertar(int pData);
+    void eliminar(int pData);
+    nodo* getCabeza();
+    void ver();
+
 };
-
-
-template <class T> void Lista<T>::insertar(T *pData)
-{
-    if(cabeza == NULL)
-    {
-        cabeza = new Nodo<T>(pData);
-    }
-    else
-    {
-        Nodo<T>* tmp = cabeza;
-        cabeza = new Nodo<T>(pData);
-        cabeza->setSiguiente() = tmp;
-    }
-}
-
-
 #endif // LISTA_H
