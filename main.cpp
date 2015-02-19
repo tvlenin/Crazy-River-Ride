fb6ce2aa766d23d09749ad89243ce42006d5f0b7
Abrahamon <ipseabraham@gmail.com>
2015-02-19 17:36:41 -0600

    main con pruebas unitarias

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
-#include "Speaker.h"
+#include <QCoreApplication>
+#include <iostream>
 
-using namespace Hello;
+#include "lista.h"
+#include "nodo.h"
+
 using namespace std;
 
-namespace Hello {
-  void Speaker::sayHello() {
-    cout << "Hello, world!\n";
-  }
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    lista l;
+
+    l.insertar(1);
+    l.insertar(2);
+    l.insertar(3);
+
+    l.eliminar(3);
+
+    cout<<"\n";
+    l.ver();
+    return a.exec();
 }
